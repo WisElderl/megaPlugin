8300575bd72f952998cdd621c876b1fa6d72d48e
GitHub Action <action@github.com>
2023-01-03 12:45:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/wiselderl/megaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wiselderl/megaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wiselderl.megaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wiselderl.megaplugin.MyBundle
 
 class MyApplicationService {
 
